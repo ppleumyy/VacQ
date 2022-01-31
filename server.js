768ca7d1b127d215cdd187ca61a93cc307b15cf0
Penpleum <pantaporn.bj@gmail.com>
2022-01-31 14:31:11 +0700

    change port

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.get('/', (req,res) => {
     res.status(200).json({success:true, data:{id:1}});
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
