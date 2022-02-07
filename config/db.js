31b9e59004a0b0387f3e693da6a0a695cddf8044
Penpleum <pantaporn.bj@gmail.com>
2022-02-07 15:28:38 +0700

    change request

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const conn=await mongoose.connect(process.env.MONGO_URL,{
+    const conn = await mongoose.connect(process.env.MONGO_URL,{
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
+    
     console.log(`MongoDB Connected: ${conn.connection.host}`);
 }
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
@@ -10,15 +11,23 @@ connectDB();
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
 const hospitals = require ('./routes/hospitals');
 app.use('/api/v1/hospitals', hospitals)
+
+
+
 
 const PORT = process.env.PORT || 5002;
 
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
 
+
+
 //Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on('unhandledRejection', (err,promise) => {
     console.log(`Error: ${err.message}`);
     //Close server & exit process
     server.close(()=>process.exit(1));
