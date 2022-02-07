9ad43db8e6ac4e540394a8c71787af1fb66bc243
Penpleum <pantaporn.bj@gmail.com>
2022-02-07 15:27:12 +0700

    connect mongodb

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
+
+//Connect to database
+connectDB();
 
 const app = express();
 
@@ -10,4 +14,12 @@ const hospitals = require ('./routes/hospitals');
 app.use('/api/v1/hospitals', hospitals)
 
 const PORT = process.env.PORT || 5002;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
+
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
