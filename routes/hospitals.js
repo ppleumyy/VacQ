1d4973fe5f26b9e081fa67806997be2fc24a34b5
Penpleum <pantaporn.bj@gmail.com>
2022-04-06 13:23:55 +0700

    OpenAPI

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -5,6 +5,187 @@ const router = express.Router();
 const appointmentRouter = require('./appointments');
 const {protect, authorize} = require('../middleware/auth');
 
+//Hospitals
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Hospitals:
+ *       type: object
+ *       required:
+ *       - name
+ *       - address
+ *       properties:
+ *         id:
+ *           type: string
+ *           format: uuid
+ *           description: The auto-generated id of the hospital
+ *           example: d290f1ee-6c54-4b01-90e6-d701748f0851
+ *         ลำดับ:
+ *           type: string
+ *           description: Ordinal number
+ *         name:  
+ *           type: string
+ *           description: Hospital name
+ *         address: 
+ *           type: string
+ *           description: House No., Street, Road
+ *         district:
+ *           type: string
+ *           description: District
+ *         province:
+ *           type: string
+ *           description: province
+ *         postalcode:
+ *           type: string
+ *           description: 5-digit postal code
+ *         tel:
+ *           type: string
+ *           description: telephone number
+ *         region:
+ *           type: string
+ *           description: region
+ *       example:
+ *         id: 609bda561452242d88d36e37
+ *         ลำดับ: 121
+ *         name: Happy Hospital
+ *         address: 121 ถ.สุขุมวิท
+ *         district: บางนา
+ *         province: กรุงเทพมหานคร
+ *         postalcode: 10110
+ *         tel: 02-2187000
+ *         region: กรุงเทพมหานคร (Bangkok)
+ */
+
+//Tags
+/**
+ * @swagger
+ * tags:
+ *   name: Hospitals
+ *   description: The hospitals managing API
+ */
+
+//Get All Hospitals
+/**
+ * @swagger
+ * /hospitals:
+ *   get:
+ *     summary: Returns the list of all the hospitals
+ *     tags: [Hospitals]
+ *     responses:
+ *       200:
+ *         description: The list of the hospitals
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Hospital'
+ */
+
+//Get One Hospital
+/**
+ * @swagger
+ * /hospitals/{id}:
+ *   get:
+ *     summary: Get the hospital by id
+ *     tags: [Hospitals]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The hospital id
+ *     responses:
+ *       200:
+ *         description: The hospital description by id
+ *         contents:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Hospital'
+ *       404:
+ *         description: The hospital was not found
+ */
+
+// Add one hospital
+/**
+ * @swagger
+ * /hospitals:
+ *   post:
+ *     summary: Create a new hospital
+ *     tags: [Hospitals]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Hospital'
+ *     responses:
+ *       201:
+ *         description: The hospital was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Hospital'
+ *       500:
+ *         description: Some server error
+ */
+
+//Update the hospital
+/**
+ * @swagger
+ * /hospitals/{id}:
+ *   put:
+ *     summary: Update the hospital by the id
+ *     tags: [Hospitals]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The hospital id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Hospital'
+ *     responses:
+ *       200:
+ *         description: The hospital was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Hospital'
+ *       404:
+ *         description: The hospital was not found
+ *       500:
+ *         description: Some error happened
+ */
+
+//Delete the hospital
+/**
+ * @swagger
+ * /hospitals/{id}:
+ *   delete:
+ *     summary: Remove the hospital by id
+ *     tags: [Hospitals]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The hospital id
+ *     responses:
+ *       200:
+ *         description: The hospital was deleted
+ *       404:
+ *         description: The hospital was not found
+ */
+
 //Re-route into other resource routers
 router.use('/:hospitalId/appointments/', appointmentRouter);
 router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospital);
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -17,6 +19,26 @@ dotenv.config({path:'./config/config.env'});
 connectDB();
 
 const app = express();
+
+const swaggerOptions={
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info:{
+            title: 'Librery API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5002/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Body parser
 app.use(express.json());
